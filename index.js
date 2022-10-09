cdf98134ee23ea23608b73d256f14b26dd2f15ef
desain studio <77520318+wolf695985@users.noreply.github.com>
2022-10-09 13:35:41 +0200

    Updatet index.js

--- index.js
+++ index.js
@@ -25,18 +25,86 @@ client.on('ready', async () => {
     console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false })}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [LOGGED IN AS ${client.user.tag}]`)
 });
 
-console.log('Test');
-
 // Load all Commands
 client.commands = new Collection();
 const commandFiles = getAllFilesFilter('./commands', '.js');
 for (const file of commandFiles) {
     const command = require(file);
     client.commands.set(command.data.name, command);
-    let cmd = command.data.name.toUpperCase()
+    const cmd = command.data.name.toUpperCase();
     console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false })}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] LOADING COMMAND ${cmd}`);
 }
 
+// Load all Buttons
+client.buttons = new Collection();
+const buttonFiles = getAllFilesFilter('./buttons', '.js');
+for (const file of buttonFiles) {
+    const button = require(file);
+    client.buttons.set(button.data.name, button);
+    const btn = button.data.name.toUpperCase();
+    console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false })}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] LOADING BUTTON ${btn}`)
+}
 
+// Interaction Handler
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand() && !interaction.isButton()) return
+	
+	if (interaction.isChatInputCommand()) {
 
+		// Check if Command Exists
+		const command = client.commands.get(interaction.commandName);
+		if (!command) return;
+
+		// Execute Command
+		try {
+			await command.execute(interaction, client);
+		} catch (e) {
+			try {
+    			let message = new EmbedBuilder()
+        			.setTitle('» ERROR')
+  					.setDescription('» <:ERROR:1020414987291861022> An Error has occured while executing this Command.\nThe Error has been logged and will be fixed soon!')
+
+    			// Send Message
+				await interaction.reply({ embeds: [message.toJSON()], ephemeral: true })
+			} catch (error) {
+				return
+			}
+		}
+
+	}
+
+	if (interaction.isButton()) {
+		// Execute Button
+		try {
+			let sc = false
+
+			// Other Button Cases
+			if (sc == false) {
+				const button = client.buttons.get(interaction.customId);
+				if (!button) return;
+
+				await button.execute(interaction, client);
+			}
+
+			return;
+		} catch (e) {
+		}
+
+	}
+
+});
+
+// Deploy Commands
+const commands = [];
+for (const file of commandFiles) {
+	const command = require(file);
+	commands.push(command.data.toJSON());
+}
+const rest = new REST({ version: '9' }).setToken(config.token);
+rest.put(
+	Routes.applicationCommands("1009930233727684739"),
+	{ body: commands },
+);
+
+// Login in the Bot
 client.login(config.token);
