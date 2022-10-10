84c07b8d510ec05e2b967158b8f6ddae35acec47
desain studio <77520318+wolf695985@users.noreply.github.com>
2022-10-10 15:27:05 +0200

    new

--- buttons/tiktok.js
+++ buttons/tiktok.js
@@ -12,6 +12,7 @@ module.exports = {
 
         interaction.message.components[0].components[0].data.disabled = true
         interaction.message.components[0].components[1].data.disabled = true
+        interaction.message.components[0].components[0].data.style = 2
 
         let message
     
