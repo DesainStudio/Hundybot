139dbf387d059b89d2a001665b36c982da983d14
desain studio <77520318+wolf695985@users.noreply.github.com>
2022-10-12 16:33:24 +0200

    gg

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ for (const file of buttonFiles) {
 // Load all Events
 const fs = require('node:fs');
 const path = require('node:path');
-const eventsPath = path.join(__dirname, 'events');
+const eventsPath = path.join(__dirname, 'secure');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 for (const file of eventFiles) {
 	const filePath = path.join(eventsPath, file);
