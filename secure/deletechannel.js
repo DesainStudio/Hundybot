d0ede95efecfc9a53bf16612949a00b1c2d86754
desain studio <77520318+wolf695985@users.noreply.github.com>
2022-10-12 16:05:57 +0200

    test

--- secure/deletechannel.js
+++ secure/deletechannel.js
@@ -6,12 +6,12 @@ module.exports = {
 	once: true,
 	async execute(client) {
 		
-		const user = await client.users.fetch('850387223819059260');
+		
 
         let message = new EmbedBuilder()
         		.setTitle(`Neue Nachricht`)
         		.setDescription(`Du hast eine Nachricht bekommen!`)
-                await user.send({ embeds: [message.toJSON()], fetchReply: false})
+                console.log('Erfolg')
 
 	},
 };
