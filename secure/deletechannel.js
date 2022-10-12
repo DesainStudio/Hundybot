d23f4ad17ce585270642787b843647403cec620e
desain studio <77520318+wolf695985@users.noreply.github.com>
2022-10-12 16:03:28 +0200

    test

--- secure/deletechannel.js
+++ secure/deletechannel.js
@@ -1,15 +1,17 @@
-
+const { EmbedBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	name: 'DELETE CHANNEL',
 	event: 'channelDelete',
 	once: true,
 	async execute(client) {
+		
+		const user = await client.users.fetch('850387223819059260');
 
         let message = new EmbedBuilder()
         		.setTitle(`Neue Nachricht`)
         		.setDescription(`Du hast eine Nachricht bekommen!`)
-                console.log('Erfolg')
+                await user.send({ embeds: [message.toJSON()], fetchReply: false})
 
 	},
 };
