0dad23c96b7d8b592c604c0f2d8883574652d438
desain studio <77520318+wolf695985@users.noreply.github.com>
2022-10-15 17:02:00 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,14 +4,12 @@ module.exports = {
 	once: true,
 	async execute(client) {
 		// Connect with MongoDB and Login
-        client.on('ready', async () => {
-            await mongoose.connect(config.mongodb || '', {
-                keepAlive: true
-            });
-            if (mongoose.connect) {
-                console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false })}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [MONGOOSEDATABANK] [CONNECTION] [SUCCESSFUL]`)
-            }
-            console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false })}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [LOGGED IN AS ${client.user.tag}]`)
+        await mongoose.connect(config.mongodb || '', {
+            keepAlive: true
         });
+        if (mongoose.connect) {
+            console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false })}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [MONGOOSEDATABANK] [CONNECTION] [SUCCESSFUL]`)
+        }
+        console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false })}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [LOGGED IN AS ${client.user.tag}]`)
 	},
 };
