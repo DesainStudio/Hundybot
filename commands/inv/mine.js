eaeb670e888cc80ed8d0ee7a994e6f3834f9d0ac
desain studio <77520318+wolf695985@users.noreply.github.com>
2022-10-16 14:25:14 +0200

    Added Mine.js

---
+++ commands/inv/mine.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { EmbedBuilder } = require('@discordjs/builders');
+const { groupCollapsed } = require('node:console');
+const wait = require('node:timers/promises').setTimeout
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('inventar')
+    	.setDMPermission(false)
+        .setDescription('Sehe dein Inventar!'),
+    async execute(interaction, client) {
+
+        // Create Embed
+        let message = new EmbedBuilder()
+        		.setTitle('Ping')
+        		.setDescription(`Du hast ${gold} Gold und ${diamant} Diamanten!`)
+            console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false})}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [USED COMMAND] [MESSAGES.JS]`);
+
+            // Send Message
+            await interaction.reply({ embeds: [message]});
+
+        // Set random Loot
+        while (true) {
+            wait(5000)
+            golds.add(interaction.user.id, 1)
+        }
+    },
+};
