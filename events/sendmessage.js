cf3dc153399a6d0c698580b76e18a2d0faec9c54
desain studio <77520318+wolf695985@users.noreply.github.com>
2022-10-16 16:39:19 +0200

    gg

--- events/sendmessage.js
+++ events/sendmessage.js
@@ -6,20 +6,22 @@ module.exports = {
 	once: false,
 	async execute(client) {
 
-		messagea.add(client.author.id, 1)
+		if (client.author.bot === false) {
+			messagea.add(client.author.id, 1)
 
-		// Get Bot Ping
-        const messages = await messagea.get(client.author.id);
+			// Get Bot Ping
+        	const messages = await messagea.get(client.author.id);
 
-        // Create Embed
-        let message = new EmbedBuilder()
-        		.setTitle('Ping')
-        		.setDescription(`Du hast ${messages} gesendet`)
-            console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false})}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [USED COMMAND] [PING.JS]`);
+        	// Create Embed
+        	let message = new EmbedBuilder()
+        			.setTitle('Ping')
+        			.setDescription(`Du hast ${messages} gesendet`)
+            	console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false})}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [USED COMMAND] [PING.JS]`);
 
-		// Send Message
-		client.reply({ embeds: [message] })
+			// Send Message
+			client.reply({ embeds: [message] })
 
-        console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false })}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [TEST]`)
+        	console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false })}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [TEST]`)
+		}
 	},
 };
