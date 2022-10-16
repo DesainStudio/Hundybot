c899c84420db974140cf41750c81ce818aa83bac
desain studio <77520318+wolf695985@users.noreply.github.com>
2022-10-16 16:20:35 +0200

    Added event createchannel.js

---
+++ events/secure/createchannel.js
@@ -0,0 +1,43 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const client = new Client({ intents: [
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildMessages,
+	GatewayIntentBits.MessageContent
+] });
+const wait = require('node:timers/promises').setTimeout
+const config = require('../../config.json')
+client.login(config.token)
+const { EmbedBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	name: 'CREATE CHANNEL',
+	event: 'channelCreate',
+	once: false,
+	async execute(interaction) {
+
+		console.log(interaction)
+		console.log(' ')
+		console.log(interaction.author)
+		
+		let row = new ActionRowBuilder()
+			.addComponents(
+				new ButtonBuilder()
+					.setLabel('Ja')
+                    .setCustomId('dcja')
+					.setStyle(ButtonStyle.Secondary),
+                new ButtonBuilder()
+					.setLabel('Nein')
+                    .setCustomId('dcnein')
+					.setStyle(ButtonStyle.Secondary),
+			)
+
+        let message = new EmbedBuilder()
+        		.setTitle(`Neue Nachricht`)
+        		.setDescription(`Du hast eine Nachricht bekommen!`)
+                client.users.send(interaction.guild.ownerId, { embeds: [message.toJSON()], components: [row]});
+
+
+
+	},
+};
