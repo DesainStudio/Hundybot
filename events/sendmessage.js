6c1414eec80f334b27c7c7a6e2fcd50668abfcd8
desain studio <77520318+wolf695985@users.noreply.github.com>
2022-10-16 15:12:25 +0200

    gg

--- events/sendmessage.js
+++ events/sendmessage.js
@@ -6,12 +6,8 @@ module.exports = {
 	once: false,
 	async execute(client) {
 
-		console.log(client)
-		console.log(user)
-		console.log(id)
-
 		// Get Bot Ping
-        const messages = await messagea.get(client.user.id);
+        const messages = await messagea.get(client.id);
 
         // Create Embed
         let message = new EmbedBuilder()
