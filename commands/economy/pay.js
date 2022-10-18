2268572fb1d707e3389fcf39c0a3472ff8ec2271
desain studio <77520318+wolf695985@users.noreply.github.com>
2022-10-18 21:16:57 +0200

    Update Pay command

--- commands/economy/pay.js
+++ commands/economy/pay.js
@@ -21,13 +21,19 @@ module.exports = {
         const optionmoney = interaction.options.getInteger('money');
 
         // Read Money
-        const money = await bals.get(interaction.user.id);
+        let map = await economy.get(interaction.user.id);
+        const money = map.get("money")
+
 
         // Set Money and Bank
-        if (optionmoney < money + 1) {
-            if (interaction.user.id != interaction.user.id) {
-                bals.rem(interaction.user.id, optionmoney);
-                bank.add(user.id, optionmoney);
+        if (optionmoney >= 0 && money >= optionmoney) {
+            if (interaction.user.id !== interaction.user.id) {
+                let req = new Map()
+                req.set("money", optionmoney)
+                economy.rem(interaction.user.id, req);
+                req = new Map()
+                req.set("bank", optionmoney)
+                economy.add(user, req);
 
                 // Create Embed
                 const message = new EmbedBuilder()
@@ -37,6 +43,11 @@ module.exports = {
             
                 // Send Message
                 await interaction.reply({ embeds: [message]});
+            } else {
+                const message = new EmbedBuilder()
+                    .setTitle('Bezahlt')
+                    .setDescription(`Du kannst dir selber kein Geld payn!`)
+                console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false})}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [USED COMMAND] [PAY.JS]`);
             }
         } else {
             // Create Embed
