7cd1bbcec81f504aab605adfe7d41171298d3f8d
desain studio <77520318+wolf695985@users.noreply.github.com>
2022-10-19 21:28:09 +0200

    Add useroption in bal.js

--- commands/economy/bal.js
+++ commands/economy/bal.js
@@ -5,24 +5,32 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('bal')
         .setDMPermission(false)
-        .setDescription('Sehe dein Konto an'),
+        .setDescription('Sehe dein Konto an')
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('THE USER')
+                .setRequired(true)
+                ),
     async execute(interaction, client) {
 
+        // Get User Option
+        const useroption = interaction.option.getUserOption('user');
+
         // Read Money
-        let map = await economy.get(interaction.user.id);
+        let map = await economy.get(useroption.id);
         const money = map.get("money")
 
         // Read Bank
-        map = await economy.get(interaction.user.id);
+        map = await economy.get(useroption.id);
         const bank = map.get("bank")
 
         // Read Kredit
-        map = await economy.get(interaction.user.id);
+        map = await economy.get(useroption.id);
         const kredit = map.get("kredit")
 
         // Create Embed
         const message = new EmbedBuilder()
-            .setTitle(`Konto von ${interaction.user.username}`)
+            .setTitle(`Konto von ${useroption.id}`)
             .setDescription(`Money: \n \n ${money} \n \n Bank: \n \n ${bank} \n \n Kredit: \n \n ${kredit} \n \n Total: \n \n ${money + bank + kredit}`)
         console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false})}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [USED COMMAND] [BAL.JS]`);
         
