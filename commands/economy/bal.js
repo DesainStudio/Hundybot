214917ad4e1e1592f86ef5c35dfdb81fa895e15a
desain studio <77520318+wolf695985@users.noreply.github.com>
2022-10-19 14:48:36 +0200

    gg

--- commands/economy/bal.js
+++ commands/economy/bal.js
@@ -23,7 +23,7 @@ module.exports = {
         // Create Embed
         const message = new EmbedBuilder()
             .setTitle(`Konto von ${interaction.user.username}`)
-            .setDescription(`Money: \n \n ${money} \n \n Bank: \n \n ${bank} \n \n Kredit: \n \n ${kredit} \n \n Total: \n \n ${money + bank}`)
+            .setDescription(`Money: \n \n ${money} \n \n Bank: \n \n ${bank} \n \n Kredit: \n \n ${kredit} \n \n Total: \n \n ${money + bank + kredit}`)
         console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false})}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [USED COMMAND] [BAL.JS]`);
         
         // Send Message
