f132e7ffc45d63ea38090b09a184b7d028f88f8e
desain studio <77520318+wolf695985@users.noreply.github.com>
2022-10-19 15:06:03 +0200

    Update inv.js

--- commands/inv/inv.js
+++ commands/inv/inv.js
@@ -9,14 +9,22 @@ module.exports = {
         .setDescription('Sehe dein Inventar!'),
     async execute(interaction, client) {
 
-        // Get Loot
-        const gold = await golds.get(interaction.user.id);
-        const diamant = await diamants.get(interaction.user.id)
+        map = await economy.get(interaction.user.id);
+        const kupfer = map.get("kupfer")
+
+        map = await economy.get(interaction.user.id);
+        const gold = map.get("gold")
+
+        map = await economy.get(interaction.user.id);
+        const dias = map.get("diamands")
+
+        map = await economy.get(interaction.user.id);
+        const emerald = map.get("emerald")
 
         // Create Embed
         let message = new EmbedBuilder()
         		.setTitle('Ping')
-        		.setDescription(`Du hast ${gold} Gold und ${diamant} Diamanten!`)
+        		.setDescription(`Kupfers: \n \n ${kupfer} \n \n Golds: \n \n ${gold} \n \n Diamanten: \n \n ${dias} \n \n Emeralds: \n \n ${emerald}`)
             console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false})}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [USED COMMAND] [MESSAGES.JS]`);
 
             // Send Message
