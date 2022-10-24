2d239bc851ba47bc9e7f8fba0641c217c8d2ed17
desain studio <77520318+wolf695985@users.noreply.github.com>
2022-10-24 23:24:28 +0200

    sdfghj

--- index.js
+++ index.js
@@ -84,6 +84,8 @@ client.on('interactionCreate', async interaction => {
 
 // Console Commands
 const economySchema = require('./schemas/economy');
+const itemSchema = require('./schemas/items');
+const economy = require('./schemas/economy');
 const stdin = process.openStdin();
 stdin.addListener("data", async function(input) {
     // Get Arguments
@@ -114,13 +116,37 @@ stdin.addListener("data", async function(input) {
 		for (const db of banks) {
 			
 			// Read Kredit
-			const bank = db.kredit;
+			const bank = db.bank;
 
 			let req = new Map()
             req.set("kredit", 1)
             economy.add(db.userId, req);
 		}
     }
+});
+stdin.addListener("data", async function(input) {
+    // Get Arguments
+    const args = input.toString().trim().split(" ")
+    console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false })}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] RECIEVED COMMAND ${input.toString().trim().toUpperCase()}`)
+
+    // Items Update
+	if (args[0].toUpperCase() == 'MINEUPDATE') {
+		// get Items 
+		const items = await itemSchema.find({})
+
+		for (const db of items) {
+
+			// get items
+			const kupfer = db.kupfer;
+			const gold = db.gold;
+			const diamant = db.diamant;
+			const emeralds = db.emerald;
+
+			let req = new Map()
+			req.set("gold", 1)
+			economy.add(db.userId, req)
+		}
+	}
 });
 
 // Deploy Commands
