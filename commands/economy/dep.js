e93e7c1fe3e7aed6b502b791df1fc20a033b237d
desain studio <77520318+wolf695985@users.noreply.github.com>
2022-12-14 14:37:42 +0100

    Update dep, with command

--- commands/economy/dep.js
+++ commands/economy/dep.js
@@ -4,52 +4,87 @@ const { EmbedBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('dep')
-    	.setDMPermission(false)
-        .setDescription('Hole geld von deiner Bank')
+        .setDMPermission(false)
+        .setDescription('Sehe dein Konto an')
         .addIntegerOption(option =>
             option.setName('money')
-                .setDescription('Betrag')
-                .setRequired(true)),
+                .setDescription('Betrag')),
     async execute(interaction, client) {
 
-        // Get Option Money
         const optionmoney = interaction.options.getInteger('money');
 
         const usereconomy = await economy2.get(interaction.user.id);
 
-        // Set Money and Bank
-        if (optionmoney >= 0 && usereconomy.money >= optionmoney) {
+        if (!optionmoney) {
 
-            economy2.edt(interaction.user.id, {
-                money: {
-                    opt: "rem",
-                    val: optionmoney
-                },
-                bank: {
-                    opt: "add",
-                    val: optionmoney
-                }
-            })
+            if (usereconomy.money >= 1) {
 
-            // Create Embed
-            const message = new EmbedBuilder()
-                .setTitle('Einzahlung')
-                .setDescription(`Du hast ${optionmoney} eingezahlt!`)
-            console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false})}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [USED COMMAND] [DEP.JS]`);
-            
-            // Send Message
-            await interaction.reply({ embeds: [message], ephemeral: true });
+                economy2.edt(interaction.user.id, {
+                    money: {
+                        opt: "rem",
+                        val: usereconomy.money
+                    },
+                    bank: {
+                        opt: "add",
+                        val: usereconomy.money
+                    }
+                })
+    
+                // Create Embed
+                const message = new EmbedBuilder()
+                    .setTitle('Einzahlung')
+                    .setDescription(`Du hast alles eingezahlt`)
+                console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false})}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [USED COMMAND] [DEP.JS]`);
+                
+                // Send Message
+                await interaction.reply({ embeds: [message], ephemeral: true });
+            } else {
+    
+                // Create Embed
+                const message = new EmbedBuilder()
+                    .setTitle('Error')
+                    .setDescription('Du hast nicht genug Money um was einzuzahlen!')
+                console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false})}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [USED COMMAND] [DEP.JS]`);
+                
+                // Send Message
+                await interaction.reply({ embeds: [message], ephemeral: true });
+            }
+
         } else {
 
-            // Create Embed
-            const message = new EmbedBuilder()
-                .setTitle('Error')
-                .setDescription('Du hast nicht genug Money um was einzuzahlen!')
-            console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false})}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [USED COMMAND] [DEP.JS]`);
-            
-            // Send Message
-            await interaction.reply({ embeds: [message], ephemeral: true });
-        }
+            if (optionmoney >= 0 && usereconomy.money >= optionmoney) {
 
-    },
-};
+                economy2.edt(interaction.user.id, {
+                    money: {
+                        opt: "rem",
+                        val: optionmoney
+                    },
+                    bank: {
+                        opt: "add",
+                        val: optionmoney
+                    }
+                })
+    
+                // Create Embed
+                const message = new EmbedBuilder()
+                    .setTitle('Einzahlung')
+                    .setDescription(`Du hast ${optionmoney} eingezahlt!`)
+                console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false})}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [USED COMMAND] [DEP.JS]`);
+                
+                // Send Message
+                await interaction.reply({ embeds: [message], ephemeral: true });
+            } else {
+    
+                // Create Embed
+                const message = new EmbedBuilder()
+                    .setTitle('Error')
+                    .setDescription('Du hast nicht genug Money um was einzuzahlen!')
+                console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false})}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [USED COMMAND] [DEP.JS]`);
+                
+                // Send Message
+                await interaction.reply({ embeds: [message], ephemeral: true });
+            }
+
+        }
+    }
+}
--- commands/economy/with.js
+++ commands/economy/with.js
@@ -4,51 +4,87 @@ const { EmbedBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('with')
-    	.setDMPermission(false)
-        .setDescription('Hole geld von deiner Bank')
+        .setDMPermission(false)
+        .setDescription('Sehe dein Konto an')
         .addIntegerOption(option =>
             option.setName('money')
-                .setDescription('Betrag')
-                .setRequired(true)),
+                .setDescription('Betrag')),
     async execute(interaction, client) {
 
-        // Get Option Money
         const optionmoney = interaction.options.getInteger('money');
 
         const usereconomy = await economy2.get(interaction.user.id);
 
-        // Set Bank and Money
-        if (optionmoney >= 0 && usereconomy.bank >= optionmoney) {
-            
-            economy2.edt(interaction.user.id, {
-                money: {
-                    opt: "add",
-                    val: optionmoney
-                },
-                bank: {
-                    opt: "rem",
-                    val: optionmoney
-                }
-            })
+        if (!optionmoney) {
 
-            // Create Embed
-            const message = new EmbedBuilder()
-                .setTitle('Auszahlen')
-                .setDescription(`Du hast ${optionmoney} ausgezahlt!`)
-            console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false})}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [USED COMMAND] [WITH.JS]`);
+            if (usereconomy.bank >= 1) {
 
-            // Send Message
-            await interaction.reply({ embeds: [message], ephemeral: true });
+                economy2.edt(interaction.user.id, {
+                    money: {
+                        opt: "add",
+                        val: usereconomy.bank
+                    },
+                    bank: {
+                        opt: "rem",
+                        val: usereconomy.bank
+                    }
+                })
+    
+                // Create Embed
+                const message = new EmbedBuilder()
+                    .setTitle('Einzahlung')
+                    .setDescription(`Du hast alles ausgezahlt`)
+                console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false})}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [USED COMMAND] [DEP.JS]`);
+                
+                // Send Message
+                await interaction.reply({ embeds: [message], ephemeral: true });
+            } else {
+    
+                // Create Embed
+                const message = new EmbedBuilder()
+                    .setTitle('Error')
+                    .setDescription('Du hast nicht genug auf der Bank um was auszuzahlen!')
+                console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false})}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [USED COMMAND] [DEP.JS]`);
+                
+                // Send Message
+                await interaction.reply({ embeds: [message], ephemeral: true });
+            }
+
         } else {
 
-            // Create Embed
-            const message = new EmbedBuilder()
-                .setTitle('Error')
-                .setDescription('Du hast nicht genug Money um was auszuzahlen!')
-            console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false})}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [USED COMMAND] [WITH.JS]`)
-            
-            // Send Message
-            await interaction.reply({ embeds: [message], ephemeral: true })
+            if (optionmoney >= 0 && usereconomy.bank >= optionmoney) {
+
+                economy2.edt(interaction.user.id, {
+                    money: {
+                        opt: "add",
+                        val: optionmoney
+                    },
+                    bank: {
+                        opt: "rem",
+                        val: optionmoney
+                    }
+                })
+    
+                // Create Embed
+                const message = new EmbedBuilder()
+                    .setTitle('Einzahlung')
+                    .setDescription(`Du hast ${optionmoney} ausgezahlt!`)
+                console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false})}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [USED COMMAND] [DEP.JS]`);
+                
+                // Send Message
+                await interaction.reply({ embeds: [message], ephemeral: true });
+            } else {
+    
+                // Create Embed
+                const message = new EmbedBuilder()
+                    .setTitle('Error')
+                    .setDescription('Du hast nicht genug auf der Bank um was auszuzahlen!')
+                console.log(`[HUNDY BOT] [i] [TIME] [${new Date().toLocaleTimeString('en-US', { hour12: false})}] [DATE] [${new Date().toLocaleDateString('de-EU', { hour12: false })}] [INF] [USED COMMAND] [DEP.JS]`);
+                
+                // Send Message
+                await interaction.reply({ embeds: [message], ephemeral: true });
+            }
+
         }
-    },
-};
+    }
+}
